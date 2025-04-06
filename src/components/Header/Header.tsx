b93699d28e8dc393f9f6ac72c06eecafbfdb0660
Nataliia Pashchenko Frontend Developer <94239256+NataPas2021@users.noreply.github.com>
2025-04-06 12:15:15 +0200

    Setup husky, prettier, eslint, structure of the project and 3 components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,10 @@
-// import React from "react";
-import styles from "./Header.module.css"; 
+import styles from './Header.module.css';
 
 const Header = () => (
-  <header className={styles.headerContainer}> 
-    <h1 className={styles.headerTitle}>Ignisia</h1> 
+  <header className={styles.headerContainer}>
+    <h1 className={styles.headerTitle}>Ignisia</h1>
     {/* Можна додати кнопки, наприклад, для авторизації */}
-    <button className={styles.headerButton}>Log In</button> 
+    <button className={styles.headerButton}>Log In</button>
   </header>
 );
 
--- src/components/IdeaList/IdeaList.tsx
+++ src/components/IdeaList/IdeaList.tsx
@@ -1,20 +1,21 @@
-// import React from "react";
-import styles from "./IdeaList.module.css"; 
+import styles from './IdeaList.module.css';
 
-const IdeaList = ({ ideas }: { ideas: string[] }) => (
-  <div className={styles.ideaListContainer}> 
-    {ideas.length === 0 ? (
-      <p className={styles.noIdeasMessage}>No ideas yet!</p> 
-    ) : (
-      <ul>
-        {ideas.map((idea, index) => (
-          <li key={index} className={styles.ideaListItem}> 
-            {idea}
-          </li>
-        ))}
-      </ul>
-    )}
-  </div>
-);
+const IdeaList = ({ ideas }: { ideas: string[] }) => {
+  return (
+    <div className={styles.ideaListContainer}>
+      {ideas.length === 0 ? (
+        <p className={styles.noIdeasMessage}>No ideas yet!</p>
+      ) : (
+        <ul>
+          {ideas.map((idea, index) => (
+            <li key={index} className={styles.ideaListItem}>
+              {idea}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default IdeaList;
